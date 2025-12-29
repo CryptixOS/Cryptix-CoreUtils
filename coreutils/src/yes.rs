da2458fd44017ac3cffe4ea2e2dc5b4a07ecdf17
0xhashcat <231815834+0xhashcatx0@users.noreply.github.com>
2025-12-29 13:53:36 +0100

    yes option update: help + version

--- coreutils/src/yes.rs
+++ coreutils/src/yes.rs
@@ -1,4 +1,25 @@
+const HELP_MESSAGE: &str = "Usage: yes [STRING]...
+  or:  yes OPTION
+Repeatedly output a line with all specified STRING(s), or 'y'.
+
+      -h , --help        display this help and exit
+      -v    , --version     output version information and exit";
+
+const VERSION_MESSAGE: &str = env!("CARGO_PKG_VERSION");
+
 pub fn yes(args: Vec<String>) -> i32 {
+	if args.iter().any(|a| a == "--help" || a == "-h") {
+		println!("{}", HELP_MESSAGE);
+
+		return 0;
+	}
+
+	if args.iter().any(|a| a == "--version" || a == "-v") {
+		println!("{}", VERSION_MESSAGE);
+
+		return 0;
+	}
+
     let output = args
         .get(1..)
         .filter(|a| !a.is_empty())
